2256714abc9341481aaaa98bc649369d49899b00
matsebe.makhura@wundermanthompson.com <matsebe.makhura@wundermanthompson.com>
2025-10-13 20:05:03 +0200

    fix

--- dist/components.js
+++ dist/components.js
@@ -1,11 +1,8 @@
 (function (global, factory) {
-  typeof exports === "object" && typeof module !== "undefined"
-    ? factory(exports)
-    : typeof define === "function" && define.amd
-    ? define(["exports"], factory)
-    : ((global = typeof globalThis !== "undefined" ? globalThis : global || self), factory((global.ATComponents = {})));
-})(this, function (exports) {
-  "use strict";
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.ATC = {}));
+})(this, (function (exports) { 'use strict';
 
   // src/components/button/button.js
   const DEFAULTS = {
@@ -78,6 +75,7 @@
   exports.ATComponents = ATComponents;
   exports.default = ATComponents;
 
-  Object.defineProperty(exports, "__esModule", { value: true });
-});
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
 //# sourceMappingURL=components.js.map

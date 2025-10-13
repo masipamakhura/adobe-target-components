b539301a33107758bed9306bbfba3d5877aa6404
matsebe.makhura@wundermanthompson.com <matsebe.makhura@wundermanthompson.com>
2025-10-13 19:50:18 +0200

    include dist files

--- src/index.js
+++ src/index.js
@@ -10,11 +10,12 @@ const ATComponents = {
 };
 
 // also attach a helper to register a new component at runtime
-// ATComponents.register = function (name, comp) {
-//   if (!name || !comp) throw new Error("register(name, component)");
-//   this[name] = comp;
-// };
+ATComponents.register = function (name, comp) {
+  if (!name || !comp) throw new Error("register(name, component)");
+  this[name] = comp;
+};
 
-export default ATComponents;
+export default { ...ATComponents };
 
 // also allow named export
+export { ATComponents };

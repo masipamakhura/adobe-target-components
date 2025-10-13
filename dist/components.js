68c1bac4725db3c8e631c76c7ee427690cfb8b83
matsebe.makhura@wundermanthompson.com <matsebe.makhura@wundermanthompson.com>
2025-10-13 19:34:53 +0200

    button fix

--- dist/components.js
+++ dist/components.js
@@ -73,6 +73,6 @@
 
   exports.ATComponents = ATComponents;
 
-  Object.defineProperty(exports, "__esModule", { value: true });
+  Object.defineProperty(ATComponents, "__esModule", { value: true });
 });
 //# sourceMappingURL=components.js.map

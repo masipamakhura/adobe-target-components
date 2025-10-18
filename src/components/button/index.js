dc5715a305f1e18a47a0d120ba029fd8b58f0dd0
matsebe.makhura@wundermanthompson.com <matsebe.makhura@wundermanthompson.com>
2025-10-18 13:04:51 +0200

    clean folder

--- src/components/button/index.js
+++
@@ -1,6 +0,0 @@
-// src/components/button/index.js
-import Button from "./button.js";
-import "./button.css"; // this will be handled by rollup/postcss (see config below)
-
-export { Button };
-export default Button;
